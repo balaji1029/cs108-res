7fe6b52193a6265d1e5426a1e1dc9e811b85b087
“AdityaNeeraje” <aditya.neeraje@gmail.com>
2024-04-10 10:28:27 +0530

    Added questions inside the solution files

--- Semester 2 labs/Python/casino.py
+++ Semester 2 labs/Python/casino.py
@@ -1,4 +1,36 @@
 '''
+Once upon a time, you were in a cool casino called "Dicey Haven". It was loud with dice rolling and people having a good time. You took a break from your usual coding stuff and decided to try a game (or your luck?).
+
+You found a table with a sign that said, "Dice Sum Challenge: Count the Ways!" A friendly person explained the task: count how many ways you can get a sum 'n' by rolling a dice multiple times, where each roll gives you a number from 1 to 6.
+
+Thinking about it for a bit, you figured you needed a Python script to solve this. Please write this python script in main.py.
+
+Usage: python3 main.py {n}
+
+(The number n will be provided as a command line argument.)
+
+
+
+For example, if you run: "python3 main.py 3". Your output should be a single integer 4.
+
+As, there are 4 ways to form the number 3: (1+1+1, 1+2, 2+1, 3).
+
+
+
+Also, you need to print your result modulo 1000000007. (1e9+7)
+
+
+
+(Note: To pass all the testcases, your code should not only produce correct results, but also run efficiently. Those who are curious can use dynamic programming. Check: https://www.geeksforgeeks.org/dynamic-programming/)
+
+(If you do not understand the Dynamic Programming Solution, then don't worry. You can submit your bruteforce solution too. But, it should pass 4 out of 10 testcases.)
+
+Note: Your code should not only produce correct results, but should run efficiently.
+
+(Hint: You can use dynamic programming. Check: https://www.geeksforgeeks.org/dynamic-programming/)
+'''
+
+'''
     Let's go to casino
     Author: Saksham Rathi
 '''
--- Semester 2 labs/Python/customCounter.py
+++ Semester 2 labs/Python/customCounter.py
@@ -1,3 +1,116 @@
+'''
+************Custom Character Counter*************
+
+Write a python program that counts characters in a given file and print the stats in a log file.
+
+
+
+************Problem Statement*************
+
+
+
+Write a python program that takes filename as command line argument. If the filename is not given as command line argument the program should print exactly,
+
+
+
+"""
+
+Filename not provided.
+
+Usage: python3 customCounter.py <file>
+
+"""(without quotations)
+
+
+
+The program should count occurrences of all the alphanumeric characters (i.e. A-Z, a-z, and 0-9) that are present in the file including common special characters belonging to [ `~!@#$%^&*()_+-=[]{}\|;'".>,</?]. Also the "A" and "a" are different characters, meaning the counting should be case sensitive. Once counting is done the program should output the stats (i.e. character and its count) in ":" separated form on standard output.
+
+
+
+Testrun Command: python3 customCounter.py file1.txt >> output.log
+
+
+
+************Example*************
+
+Assume file1.txt has following text:
+
+"""
+
+I am Saksham Rathi. e-mail:sakshamrathi21@gmail.com
+
+"""(without quotations)
+
+
+
+Then the output should contain:
+
+
+
+"""
+
+I:1
+
+ :4
+
+a:9
+
+m:6
+
+S:1
+
+k:2
+
+s:3
+
+h:4
+
+R:1
+
+t:2
+
+i:4
+
+.:2
+
+e:1
+
+-:1
+
+l:2
+
+r:1
+
+2:1
+
+1:1
+
+@:1
+
+g:1
+
+c:1
+
+o:1
+
+"""(without quotations)
+
+
+
+Please note that "A" and "a" have 2 different entries, also " "(space) as a character is also part of the output. Please also note that any other characters other than given in the problem statement will not be part of the testcases too (even newline characters will not be in testcases).
+
+
+
+
+
+For command line argument handling, you will be required to use sys library. (It is already imported for you)
+
+You can check these links to learn more about this library:
+
+https://docs.python.org/3/library/sys.html
+https://www.geeksforgeeks.org/python-sys-module/
+
+'''
 import sys
 import string
 
